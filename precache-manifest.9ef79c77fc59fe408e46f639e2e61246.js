32b87b9c2658606db97505ca9ea3dd6ab1aad904
sasha.kalinskiy@yandex.ru <jsn364cmd025>
2021-02-03 15:52:03 +0300

    Updates

---
+++ precache-manifest.9ef79c77fc59fe408e46f639e2e61246.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25a575976b790f35ee4f7098cb9dd9c",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "571372633c208ce0665b",
+    "url": "/todo/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "d47dd5faeff513420cec",
+    "url": "/todo/static/js/2.62b20642.chunk.js"
+  },
+  {
+    "revision": "3e5bb88f3f2708c5dfec1e159b6a3a23",
+    "url": "/todo/static/js/2.62b20642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "571372633c208ce0665b",
+    "url": "/todo/static/js/main.0d86ecf9.chunk.js"
+  },
+  {
+    "revision": "e9b3b084cc17457a6264",
+    "url": "/todo/static/js/runtime-main.c58ac565.js"
+  }
+]);
